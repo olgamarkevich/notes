f97829cc6cb67bc4ca0221ac755450f714669d13
Olga Markevich <pyrkoolga@gmail.com>
2023-12-13 13:52:54 +0300

    fix

--- src/store/notes/model.ts
+++ src/store/notes/model.ts
@@ -3,20 +3,12 @@ export interface Note {
   text: string;
 }
 
-export interface Alert {
-  id: number;
-  type: string;
-  message: string;
-}
-
 export interface NotesState {
   items: Note[];
   tagsFilter: string[];
-  alerts: Alert[];
 }
 
 export const initialState: NotesState = {
   items: [],
   tagsFilter: [],
-  alerts: [],
 };
